6a1e399da16d0c5053a5d121143e3180a2d4da22
NoeSourdes <sourdesnoe19@gmail.com>
2024-05-14 22:30:29 +0200

    Update PDF preview scaling and add responsive scaling in PreviewPdf component

--- src/components/dashboard/preview_lesson/top-bar-pdf.tsx
+++ src/components/dashboard/preview_lesson/top-bar-pdf.tsx
@@ -1,14 +1,51 @@
+"use client";
+
+import { ArrowLeft, ArrowRight, Expand } from "lucide-react";
+import { useState } from "react";
+import { Button } from "../../ui/button";
+
 export type TopBarPdfProps = {
   setNumPages: (numPages: number) => void;
   numPages: number | undefined;
   setPageNumber: (pageNumber: number) => void;
   pageNumber: number;
+  isFullScreen: boolean;
+  setIsFullScreen: (isFullScreen: boolean) => void;
 };
 
 export const TopBarPdf = (props: TopBarPdfProps) => {
   return (
     <div className="h-14 w-full border-b flex items-center justify-between rounded-md border p-2">
-      <div className="flex items-center gap-1.5">Top bar</div>
+      <div className="flex items-center gap-1.5">
+        <Button
+          size="icon"
+          variant="secondary"
+          onClick={() => props.setPageNumber(props.pageNumber - 1)}
+          disabled={props.pageNumber === 1}
+        >
+          <ArrowLeft size={20} />
+        </Button>
+        <Button
+          size="icon"
+          variant="secondary"
+          onClick={() => props.setPageNumber(props.pageNumber + 1)}
+          disabled={props.pageNumber === props.numPages}
+        >
+          <ArrowRight size={20} />
+        </Button>
+        <span>
+          Page {props.pageNumber} sur {props.numPages}
+        </span>
+      </div>
+      <div>
+        <Button
+          size="icon"
+          variant="secondary"
+          onClick={() => props.setIsFullScreen(!props.isFullScreen)}
+        >
+          <Expand size={20} />
+        </Button>
+      </div>
     </div>
   );
 };
