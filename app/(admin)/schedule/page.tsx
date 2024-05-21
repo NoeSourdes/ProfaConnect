d7b51530e31f4abcae108369dd7b2d6a1f403cf6
NoeSourdes <sourdesnoe19@gmail.com>
2024-05-21 23:10:28 +0200

    Fix layout and sidebar styling

--- app/(admin)/schedule/page.tsx
+++ app/(admin)/schedule/page.tsx
@@ -9,10 +9,25 @@ import { useState } from "react";
 
 export default function TimeStamp() {
   const [date, setDate] = useState<Date | undefined>(new Date());
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
 
   return (
-    <div className="h-full w-full flex gap-3">
-      <section className="flex flex-col gap-3 h-full">
+    <div
+      className={`min-h-[calc(100vh-56px-64px-20px-24px-56px-48px)] w-full flex gap-3 relative overflow-hidden`}
+    >
+      <div
+        onClick={() => setSidebarIsOpen(false)}
+        className={`absolute inset-0 bg-black/25 z-20 rounded-md shadow-xl cursor-pointer transition-all ${
+          sidebarIsOpen ? "opacity-1" : "opacity-0 pointer-events-none"
+        }`}
+      ></div>
+      <section
+        className={`flex flex-col gap-3 max-[900px]:-translate-x-[19rem] transition-all bg-background z-30 pt-3 ${
+          sidebarIsOpen
+            ? "max-[900px]:translate-x-0 px-3"
+            : "max-[900px]:-translate-x-[19rem]"
+        }`}
+      >
         <div>
           <Button size="lg" className="w-full">
             Créer un événement
@@ -28,8 +43,13 @@ export default function TimeStamp() {
         <Categories />
         <div></div>
       </section>
-      <section className="grow w-full h-full">
-        <FullCalendarComponent />
+      <section
+        className={`h-full absolute max-[900px]:left-0 left-[18rem] right-0 transition-all z-10 pt-3`}
+      >
+        <FullCalendarComponent
+          setSidebarIsOpen={setSidebarIsOpen}
+          sidebarIsOpen={sidebarIsOpen}
+        />
       </section>
     </div>
   );
