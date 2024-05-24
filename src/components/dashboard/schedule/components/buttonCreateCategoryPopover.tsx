062202064da474fef825e15b925e181951bb9b12
NoeSourdes <sourdesnoe19@gmail.com>
2024-05-24 16:53:18 +0200

    Update colors and add category creation functionality

---
+++ src/components/dashboard/schedule/components/buttonCreateCategoryPopover.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { Button } from "@/src/components/ui/button";
+import { Input } from "@/src/components/ui/input";
+
+import { Plus } from "lucide-react";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/src/components/ui/popover";
+import { useState } from "react";
+import { toast } from "sonner";
+
+export type buttonCreateCategoryPopoverProps = {
+  categories: any;
+  formCategory: any;
+  formIdCategory: any;
+  mutationCategory: any;
+};
+
+export const ButtonCreateCategoryPopover = (
+  props: buttonCreateCategoryPopoverProps
+) => {
+  const [categoryName, setCategoryName] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (categoryName === "") {
+      toast.error("Le nom de la catégorie est requis");
+      return;
+    }
+    await props.mutationCategory.mutateAsync({ name: categoryName });
+    setCategoryName("");
+  };
+  return (
+    <>
+      {props.categories?.length === 0
+        ? "Aucune catégorie"
+        : "Créer une catégorie"}{" "}
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button size="icon_sm" variant="secondary">
+            <Plus />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-80">
+          <form className="flex flex-col gap-3">
+            <div>
+              <label className="text-sm font-medium" htmlFor="categoryName">
+                Titre de la catégorie
+              </label>
+              <Input
+                id="categoryName"
+                placeholder="Entrez le titre de la catégorie"
+                value={categoryName}
+                onChange={(e) => setCategoryName(e.target.value)}
+              />
+            </div>
+            <Button
+              onClick={handleSubmit}
+              disabled={props.mutationCategory.isPending}
+              className="w-full"
+            >
+              {props.mutationCategory.isPending ? "En cours..." : "Ajouter"}
+            </Button>
+          </form>
+        </PopoverContent>
+      </Popover>
+    </>
+  );
+};
