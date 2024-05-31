f2a712634384825a15f1f6e2e996475ccdaed6a7
NoeSourdes <sourdesnoe19@gmail.com>
2024-05-31 14:49:20 +0200

    Add meta viewport tag and tooltip functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,13 @@ export default function RootLayout({
   return (
     <>
       <html lang="fr" suppressHydrationWarning>
-        <head />
+        <head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0 ,
+    maximum-scale=1.0 , user-scalable=no"
+          />
+        </head>
         <body className={GeistSans.className}>
           <NextTopLoader
             zIndex={1000000000000000}
