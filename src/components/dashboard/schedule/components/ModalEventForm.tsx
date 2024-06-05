009f8b2bda62f792c6666f6348b9ba76dab54060
NoeSourdes <sourdesnoe19@gmail.com>
2024-06-05 22:31:22 +0200

    Update PopoverContent styling in ModalEventForm

--- src/components/dashboard/schedule/components/ModalEventForm.tsx
+++ src/components/dashboard/schedule/components/ModalEventForm.tsx
@@ -282,7 +282,10 @@ export const ModalEventForm = (props: ModalEventFormProps) => {
                         )}
                       </Button>
                     </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0" align="start">
+                    <PopoverContent
+                      className="w-auto p-0 z-[50000]"
+                      align="start"
+                    >
                       <Calendar
                         mode="single"
                         selected={field.value}
