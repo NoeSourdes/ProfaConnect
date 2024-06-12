1e65463e16625b2649f01ee3877609417b1455e8
NoeSourdes <sourdesnoe19@gmail.com>
2024-06-12 20:05:24 +0200

    refactor: Remove role field from lesson and course schemas

--- app/(admin)/courses/[coursId]/edit/course.schema.ts
+++ app/(admin)/courses/[coursId]/edit/course.schema.ts
@@ -27,7 +27,6 @@ export const courseSchema = z.object({
       name: z.string().nullable(),
       email: z.string().nullable(),
       image: z.string().nullable(),
-      role: z.string(),
     })
     .nullable(),
 });
