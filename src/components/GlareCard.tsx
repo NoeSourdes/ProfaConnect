6560f6abe3b3de3fc105dd193d22df4e32182301
NoeSourdes <sourdesnoe19@gmail.com>
2024-06-26 22:53:09 +0200

    refactor: Update GlareCard component with user profile data and particles animation

--- src/components/GlareCard.tsx
+++ src/components/GlareCard.tsx
@@ -1,18 +1,68 @@
+import { getUserProfileAction } from "@/actions/user/user";
+import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { getRoleFrench } from "../hooks/user-actions";
 import { BackgroundGradientAnimation } from "./ui/background-gradient-animation";
 import { GlareCard } from "./ui/glare-card";
+import Particles from "./ui/particles";
 
 export function GlareCardComp() {
   const session = useSession();
+  const {
+    data: userProfile,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["user", session?.data?.user?.id ?? ""],
+    queryFn: async () => {
+      const userProfile = await getUserProfileAction(
+        session?.data?.user?.id ?? ""
+      );
+      return userProfile;
+    },
+  });
+
   return (
     <GlareCard className="bg-secondary">
+      <Particles
+        className="absolute inset-0"
+        quantity={100}
+        ease={80}
+        color={`#5BBFC8`}
+        refresh
+      />
       <div className="absolute inset-0 rounded-[15px] border-4 dark:border-[#2E3E58] p-5 flex flex-col gap-3">
-        <div className=" h-[230px] w-[280px] rounded-t-[200px] rounded-b-md relative overflow-hidden">
+        <div className="min-h-[200px] max-h-[220px] w-[280px] rounded-t-[200px] rounded-b-md relative overflow-hidden">
           <BackgroundGradientAnimation></BackgroundGradientAnimation>
         </div>
-        <p className="font-bold text-2xl text-[#6C00A2]">
-          {session.data?.user?.name}
-        </p>
+        <div className="w-full h-full flex flex-col justify-between text-[#12374A]">
+          <div>
+            <p className="font-semibold text-2xl">{session.data?.user?.name}</p>
+            <p className="font-medium">
+              {getRoleFrench(userProfile?.data?.role ?? "")}
+            </p>
+          </div>
+          <div className="flex justify-between items-center w-full">
+            <div className=" text-xs flex items-center">
+              <p className="border border-[#12374A] rounded-l px-1">
+                ProfaConnect
+              </p>
+              <span className="w-4 bg-[#12374A]/50 h-[18px] border-t border-b border-[#12374A]"></span>
+              <p className="border border-[#12374A] rounded-r px-1">
+                21 Oct 2023
+              </p>
+            </div>
+            <div>
+              <Image
+                src="/img/logo_card.svg"
+                alt="ProfaConnect"
+                width={32}
+                height={32}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </GlareCard>
   );
