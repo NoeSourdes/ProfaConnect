6fe5040ae66d996f1b32790ffd4e1a38e365576d
NoeSourdes <sourdesnoe19@gmail.com>
2024-07-04 22:17:43 +0200

    delete for editor

--- src/lib/plate-ui/is-enabled.ts
+++
@@ -1,7 +0,0 @@
-import type { ValueId } from "@/src/config/customizer-plugins";
-
-export const isEnabled = (
-  id: ValueId,
-  currentId?: ValueId,
-  componentId?: boolean
-) => (!currentId && componentId !== false) || currentId === id;
