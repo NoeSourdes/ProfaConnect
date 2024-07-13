0a7714c7fe40fb20665810157294d33ac7fe12e6
NoeSourdes <sourdesnoe19@gmail.com>
2024-07-13 23:11:08 +0200

    refactor: Remove unused files related to the stepper component

--- app/(admin)/classroom/page.tsx
+++ app/(admin)/classroom/page.tsx
@@ -1,21 +1,8 @@
 "use client";
 
 import { getClassroomsByProfessorIdAction } from "@/actions/classroom/classroom.actions";
-import { Button } from "@/src/components/ui/button";
-import {
-  Step,
-  Stepper,
-  useStepper,
-  type StepItem,
-} from "@/src/components/ui/stepper";
 import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
-
-const steps = [
-  { label: "Step 1" },
-  { label: "Step 2" },
-  { label: "Step 3" },
-] satisfies StepItem[];
 
 export default function Page() {
   const { data: session } = useSession();
@@ -34,51 +21,6 @@ export default function Page() {
     },
   });
 
-  return (
-    <div className="flex w-full flex-col gap-4">
-      <Stepper initialStep={0} steps={steps}>
-        {steps.map((stepProps, index) => {
-          return <Step key={stepProps.label} {...stepProps}></Step>;
-        })}
-        <Footer />
-      </Stepper>
-    </div>
-  );
+  console.log(classroom);
+  return <div>Ma classe</div>;
 }
-
-const Footer = () => {
-  const {
-    nextStep,
-    prevStep,
-    resetSteps,
-    hasCompletedAllSteps,
-    isLastStep,
-    isOptionalStep,
-    isDisabledStep,
-  } = useStepper();
-  return (
-    <>
-      <div className="w-full flex justify-end gap-2">
-        {hasCompletedAllSteps ? (
-          <Button size="sm" onClick={resetSteps}>
-            Reset
-          </Button>
-        ) : (
-          <>
-            <Button
-              disabled={isDisabledStep}
-              onClick={prevStep}
-              size="sm"
-              variant="secondary"
-            >
-              Prev
-            </Button>
-            <Button size="sm" onClick={nextStep}>
-              {isLastStep ? "Finish" : isOptionalStep ? "Skip" : "Next"}
-            </Button>
-          </>
-        )}
-      </div>
-    </>
-  );
-};
